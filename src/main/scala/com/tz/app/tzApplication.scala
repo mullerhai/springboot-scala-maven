8334dc7ead4ac5892e23f9dc2f9c579eece55e85
mullerhai <hai710459649@foxmail.com>
2018-10-19 16:32:50 +0800

    可以打成jar 执行 java -jar ***.jar

--- src/main/scala/com/tz/app/tzApplication.scala
+++ src/main/scala/com/tz/app/tzApplication.scala
@@ -1,7 +1,8 @@
 package com.tz.app
 
-import org.springframework.boot.{SpringApplication, SpringBootConfiguration}
+import org.springframework.boot.{Banner, SpringApplication, SpringBootConfiguration, WebApplicationType}
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.builder.SpringApplicationBuilder
 
 @SpringBootApplication
 @SpringBootConfiguration
@@ -12,9 +13,18 @@ class  tzApplication{
 object tzApplication  {
 
 
-     def main(args: Array[String]): Unit = {
-        SpringApplication.run(classOf[AppConfig])
+    def main(args:Array[String]): Unit = {
+        (new SpringApplicationBuilder)
+                .bannerMode(Banner.Mode.CONSOLE)
+                .sources(classOf[tzApplication])
+                .web(WebApplicationType.SERVLET)
+                .headless(true)
+                .run(args:_*)
     }
+
+//     def main(args: Array[String]): Unit = {
+//        SpringApplication.run(classOf[AppConfig])
+//    }
 
 
 }
